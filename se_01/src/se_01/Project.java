d88ef8943436346c307a97f0926c5cad4bae34ea
totto2s <32835534+totto2s@users.noreply.github.com>
2017-10-17 12:50:53 +0200

    1.3

---
+++ se_01/src/se_01/Project.java
@@ -0,0 +1,34 @@
+package se_01;
+
+public class Project {
+    String [][] sim ;
+    int anzahl=0;
+    
+    public  Project(String [][] prio){
+        sim = prio;
+    }
+    public boolean isWellSorted( String [] test){
+        for(int m=0; m<test.length; m++){
+            for(int n=m+1; n<test.length; n++){
+                if( test[n].equals(test[m])){
+                    return false;
+                }
+            }
+            
+        }
+        for( int i=0; i<sim.length;i++){
+            for( int j=0; j<test.length; j++){
+                if( sim[i][0].equals(test[j])){
+                    for(int k=j+1; k<test.length; k++){
+                        if((sim[i][1].equals(test[k])&&(!(sim[i][0].equals(sim[i][1]))))){
+                            anzahl+=1;
+                            
+                        }
+                    }
+                }
+            }
+        }
+        return (sim.length==anzahl);
+        
+    }
+}
